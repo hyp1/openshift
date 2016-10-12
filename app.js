4fc155f751c69d90632a1e6ccd3fe7a5ab317190
hyp1 <robert_schweng@hotmail.com>
2016-10-13 01:21:54 +0200

    Openshift Adresses and Ports

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+http.createServer(app).listen(server_port,server_ip_address , function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
